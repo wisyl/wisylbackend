55fd1c8700ebe8116ce4c6d12e69a76e3c28302c
Pro Dev <prodev9999@gmail.com>
2019-05-23 06:01:42 +0800

    Update express.js

--- config/express.js
+++ config/express.js
@@ -5,18 +5,16 @@
  */
 
 const express = require('express');
-const session = require('express-session');
 const compression = require('compression');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const csrf = require('csurf');
+//const csrf = require('csurf');
 const cors = require('cors');
 const helmet = require('helmet');
 const upload = require('multer')();
 
-const mongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const winston = require('winston');
 const helpers = require('view-helpers');
@@ -31,9 +29,9 @@ const env = process.env.NODE_ENV || 'development';
  * Expose
  */
 
-module.exports = function(app, passport) {
+module.exports = function(app) {
   app.use(helmet());
-  app.use(requireHttps);
+  //app.use(requireHttps);
 
   // Compression middleware (should be placed before express.static)
   app.use(
@@ -42,10 +40,9 @@ module.exports = function(app, passport) {
     })
   );
 
-  const host = process.env.HOST || 'wisyl.com';
   app.use(
     cors({
-      origin: [`http://localhost:3000`, `https://${host}`],
+      origin: [`http://localhost:3000`],
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
       credentials: true
     })
@@ -69,7 +66,7 @@ module.exports = function(app, passport) {
   if (env !== 'test') app.use(morgan(log));
 
   // set views path, template engine and default layout
-  app.set('views', config.root + '/app/views');
+  app.set('views', config.root + '/app/admin/views');
   app.set('view engine', 'pug');
 
   // expose package.json to views
@@ -96,21 +93,6 @@ module.exports = function(app, passport) {
 
   // CookieParser should be above session
   app.use(cookieParser());
-  app.use(
-    session({
-      resave: false,
-      saveUninitialized: true,
-      secret: pkg.name,
-      store: new mongoStore({
-        url: config.db,
-        collection: 'sessions'
-      })
-    })
-  );
-
-  // use passport session
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   // connect flash for flash messages - should be declared after sessions
   app.use(flash());
@@ -119,17 +101,13 @@ module.exports = function(app, passport) {
   app.use(helpers(pkg.name));
 
   if (env !== 'test') {
-    app.use(csrf());
+    //app.use(csrf());
 
     // This could be moved to view-helpers :-)
     app.use(function(req, res, next) {
-      res.locals.csrf_token = req.csrfToken();
+      //res.locals.csrf_token = req.csrfToken();
       res.locals.paginate = ultimatePagination.getPaginationModel;
       next();
     });
-  }
-
-  if (env === 'development') {
-    app.locals.pretty = true;
   }
 };
