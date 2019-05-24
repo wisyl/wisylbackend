6ceacafa23620894069b6218214ddf3d814f3a07
Pro Dev <prodev9999@gmail.com>
2019-05-25 05:52:18 +0800

    minor model definition update

---
+++ app/models/_createTables.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+const vogels = require('vogels');
+
+module.exports = function (model) {
+  vogels.createTables(function (err) {
+    if (err) {
+      console.log(`Error creating ${model} table: `, err);
+      return process.exit(1);
+    }
+    console.log(`${model} table has been created`);
+  });
+}
--- app/models/admin.js
+++ app/models/admin.js
@@ -3,15 +3,13 @@
 const vogels = require('vogels');
 const Joi = require('joi');
 const crypto = require('crypto');
-const uuid = require('uuid4')
 const config = require('../../config');
 vogels.AWS.config.update(config.aws);
 
 const Admin = vogels.define('Admin', {
-  hashKey: 'id',
+  hashKey: 'email',
   timestamps: true,
   schema: {
-    id: Joi.string(),
     name: Joi.string().trim().regex(/^[A-Za-z ]{3,}$/),
     email: Joi.string().email().trim().required(),
     hashed_password: Joi.string(),
@@ -27,7 +25,6 @@ Admin.before('create', function (data, next) {
   if (!data.password || !data.password.length) {
     return next(new Error('Password can not be empty'), data)
   }
-  data.id = uuid();
   data.salt = data.makeSalt();
   data.hashed_password = data.encryptPassword(data.password);
   next(null, data);
@@ -74,29 +71,22 @@ Admin.prototype.encryptPassword = function (password) {
  */
 
 Admin.load = function (options, cb) {
-  options.select = options.select || 'name email';
-  return this.findOne(options.criteria)
-    .select(options.select)
-    .exec(cb);
+  //options.select = options.select || 'name email';
+  //return this.findOne(options.criteria)
+  //  .select(options.select)
+  //  .exec(cb);
 };
 
 Admin.list = function (options) {
-  const criteria = options.criteria || {};
-  const page = options.page || 0;
-  const limit = options.limit || 30;
-  return this.find(criteria)
-    .limit(limit)
-    .skip(limit * page)
-    .exec();
+  //const criteria = options.criteria || {};
+  //const page = options.page || 0;
+  //const limit = options.limit || 30;
+  //return this.find(criteria)
+  //  .limit(limit)
+  //  .skip(limit * page)
+  //  .exec();
 };
 
-vogels.createTables(function (err) {
-  if (err) {
-    console.log('Error creating Admin table: ', err);
-    return process.exit(1);
-  }
-  console.log('Admin table has been created');
-
-});
+require('./_createTables')('Admin');
 
 module.exports = Admin
